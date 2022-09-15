28c857887de08343e3d94655bdf72e37508c21d3
Freddy8358 <Freddy8358@pm.me>
2022-09-15 13:44:01 +0900

    ipv4

--- ps.sh
+++ ps.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+# update
 apt update && apt full-upgrade -y && apt autoremove -y 
 
+# remove agent
 apt --purge autoremove -y linux-azure-* linux-cloud-tools-* walinuxagent 
 pkill -9 hv_kvp_daemon
-
 apt --purge autoremove *google* -y
 
+# bbr
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 echo "net.ipv4.ip_local_port_range = 1024 65535" >> /etc/sysctl.conf
 sysctl -p
+
+# ipv4 precedence
+echo "precedence ::ffff:0:0/96  100" >> /etc/gai.conf
 
 reboot
