ef6f268c34a4936e0b69da20c70deea42128fbec
Freddy8358 <Freddy8358@pm.me>
2022-09-17 19:41:13 +0900

    bbr

---
+++ bbr.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# update
+apt update && apt full-upgrade -y && apt autoremove -y 
+
+# bbr
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl -p
+
+# ipv4 precedence
+echo "precedence ::ffff:0:0/96  100" >> /etc/gai.conf
+
+reboot
