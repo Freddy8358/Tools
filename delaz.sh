f72d5d88830bb587e854bd146bba0a556a33d054
Freddy <113289198+Freddy8358@users.noreply.github.com>
2023-01-07 18:58:02 +0800

    aws

--- delaz.sh
+++ delaz.sh
@@ -1,6 +1,11 @@
+# del az & gcp
 apt --purge autoremove -y linux-azure-* 
 apt --purge autoremove -y linux-cloud-tools-* 
 apt --purge autoremove -y waagent 
 apt --purge autoremove -y walinuxagent 
 apt --purge autoremove -y *google*
 pkill -9 hv_kvp_daemon
+
+# del aws
+apt remove amazon*
+snap remove amazon-ssm-agent
